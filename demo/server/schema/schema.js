d67daf9b660b367e72e7e5dcb7ac62268b754f1c
Khayal <alaskarov.khayal@gmail.com>
2021-07-31 16:44:02 -0700

    GraphQl Demo with simple schema, resolvers and database was created. > > Co-authored-by: Spencer Co-authored-by: Vivian>

--- demo/server/schema/schema.js
+++ demo/server/schema/schema.js
@@ -1,3 +1,26 @@
-const db = require("../models/model.js");
+const { buildSchema } = require('graphql');
 
+// Construct a schema, using GraphQL schema language
+
+const schema = buildSchema(`
+  type Person {
+    id: Int!
+    name: String!
+    mass: String!
+    hair_color: String!
+    skin_color: String!
+    birth_year: String!
+    gender: String!
+    species_id: Int!
+    homeworld_id: Int!
+    height: Int!
+  }
+
+  type Query {
+    people: [Person!]
+    person(id: Int!): Person
+  }
+`);
+
+module.exports = schema;
 
