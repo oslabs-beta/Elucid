14adab2e539a57af8cc98bbc91436eb5b2a08d45
Khayal <alaskarov.khayal@gmail.com>
2021-08-04 20:53:04 -0700

    working on 'null' fileds when status code 200 OK > > Co-authored-by: Spencer Szu 39441993+Szuay@users.noreply.github.com Co-authored-by: Vivian Wu 75869500+vw-in-github@users.noreply.github.com Co-authored-by:Gary Iacobucci 1161598+garyiacobucci@users.noreply.github.com

--- demo/server/schema/schema.js
+++ demo/server/schema/schema.js
@@ -3,23 +3,31 @@ const { buildSchema } = require('graphql');
 // Construct a schema, using GraphQL schema language
 
 const schema = buildSchema(`
-  type Person {
+type Homeworld {
+  name: String
+  climate: String
+  population: String
+}
+
+type Person {
     id: Int!
-    name: String!
+    name: String
+    height: String!
     mass: String!
     hair_color: String!
     skin_color: String!
+    eye_color: String!
     birth_year: String!
-    gender: String!
-    species_id: Int!
-    homeworld_id: Int!
-    height: Int!
+    gender: String
+    homeworld: Homeworld!
   }
+
+
 
   type Query {
     people: [Person!]
-    person(id: Int!): Person
-  }
+    person(id: Int): Person
+    }
 `);
 
 module.exports = schema;
